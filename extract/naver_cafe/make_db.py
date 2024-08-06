0c4b13d5c774822ba9e0c24cf11d44ad19ae5a80
patrashu <qhdrmfdl123@naver.com>
2024-08-06 15:45:30 +0900

    feat: 데이터프레임을 병합할 때 정렬하는 코드 추가

--- extract/naver_cafe/make_db.py
+++ extract/naver_cafe/make_db.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def merge_csv_files(path: str = '../data/*.csv') -> pd.DataFrame:
-    all_files = glob(path)
+    all_files = sorted(glob(path))
     df = pd.concat((pd.read_csv(f) for f in all_files))
     return df
 
@@ -13,6 +13,9 @@ def main():
     merged_df = merge_csv_files()
     merged_df['Community'] = 'naver_cafe'
     merged_df.drop("post_id", axis=1, inplace=True)
+    merged_df.to_csv('merged_naver_cafe_data.csv',
+                     index=False, encoding='utf-8-sig')
+
     conn = sqlite3.connect('Ioniq_naver_cafe_result.db')
 
     cursor = conn.cursor()
