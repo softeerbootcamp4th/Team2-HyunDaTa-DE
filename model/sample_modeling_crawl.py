28036f6d11631b6cb200ffe7d31a9fdc8dc6a166
patrashu <qhdrmfdl123@naver.com>
2024-08-15 16:32:03 +0900

    [feat]: RDS에 데이터가 저장되도록 코드 수정
    #14

--- model/sample_modeling_crawl.py
+++ model/sample_modeling_crawl.py
@@ -1,6 +1,7 @@
 import argparse
 import unicodedata
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit, col
 
 
 """ Sample Modeling Script
@@ -9,16 +10,30 @@ emerge score = View + Like * 20 + (Title_wc + Body_wc + Comment_wc) * 10
 """
 
 
+JDBC_URL = "db_url"
+DB_NAME = "dbname"
+DB_USER = "dbusername"
+DB_PASSWORD = "dbpassword"
+
+
 def extract_contents_contains_car_name(bucket_name: str, base_path: str, car_name: str):
-    return f"s3://{bucket_name}/{base_path}/*{car_name}*.csv"
+    return f"s3://{bucket_name}/{base_path}/*/*{car_name}*.csv"
 
 
-def save_to_rds(df, rds_url, rds_table, rds_properties):
-    df.write.jdbc(url=rds_url, table=rds_table,
-                  mode="append", properties=rds_properties)
+def save_to_rds(df, rds_url, rds_table, user, password):
+    df.coalesce(1).write.jdbc(
+        url=rds_url,
+        table=rds_table,
+        mode="append",
+        properties={
+            "user": user,
+            "password": password,
+            "driver": "com.mysql.cj.jdbc.Driver"
+        }
+    )
 
 
-def calculate_emerge_score(path_pattern: str, output_uri: str, car_name: str, issue: str):
+def calculate_emerge_score(path_pattern: str, car_name: str, issue: str):
     spark = SparkSession.builder.appName(
         f"Calculate {car_name}'s Issue: {issue}").getOrCreate()
 
@@ -30,9 +45,13 @@ def calculate_emerge_score(path_pattern: str, output_uri: str, car_name: str, is
         escape='"',
         multiLine=True,
         ignoreLeadingWhiteSpace=True,
-        ignoreTrailingWhiteSpace=True
+        ignoreTrailingWhiteSpace=True,
     )
 
+    tmp_df = tmp_df.withColumn("View", col("View").cast("int")) \
+        .withColumn("Like", col("Like").cast("int"))
+    tmp_df.show(5)
+    tmp_df.printSchema()
     tmp_df.createOrReplaceTempView("data_df")
     sql_query = f"""
     SELECT 
@@ -46,22 +65,32 @@ def calculate_emerge_score(path_pattern: str, output_uri: str, car_name: str, is
     """
 
     wc_df = spark.sql(sql_query)
+    wc_df.show(5)
+    wc_df.printSchema()
     wc_df.createOrReplaceTempView("wc_df")
     final_query = """
     SELECT 
-        Date,
-        SUM(View) AS Total_View,
-        SUM(Like) AS Total_Like,
-        SUM(Title_wc + Body_wc + Comment_wc) AS Total_wc,
-        SUM(View + Like * 20 + (Title_wc + Body_wc + Comment_wc) * 10) AS Graph_score
+        TRIM(Date) AS upload_date, 
+        SUM(View) AS total_view,
+        SUM(Like) AS total_like,
+        SUM(Title_wc + Body_wc + Comment_wc) AS total_wc,
+        total_view + total_like * 200 + total_wc * 1000 AS graph_score
     FROM wc_df
-    GROUP BY Date
-    ORDER BY Date
+    GROUP BY TRIM(Date)
+    ORDER BY TRIM(Date)
     """
 
     final_df = spark.sql(final_query)
-    final_df.coalesce(1).write.option(
-        "header", "true").mode("overwrite").csv(output_uri)
+    final_df = final_df.withColumn("issue", lit(issue))
+    final_df = final_df.withColumn("carname", lit(car_name))
+    final_df.show(5)
+    save_to_rds(
+        final_df,
+        JDBC_URL,
+        DB_NAME,
+        DB_USER,
+        DB_PASSWORD
+    )
 
     spark.stop()
 
@@ -72,8 +101,6 @@ if __name__ == "__main__":
         '--bucket_name', help="The name of the S3 bucket.")
     parser.add_argument(
         '--base_path', help="The base path of the S3 bucket.")
-    parser.add_argument(
-        '--output_uri', help="The URI where output is saved, like an S3 bucket location.")
     parser.add_argument(
         '--car_name', help="The name of the car you want to analyze. [genesis, palisade, casper, ...]")
     parser.add_argument(
@@ -86,7 +113,6 @@ if __name__ == "__main__":
     issue = unicodedata.normalize("NFC", args.issue)
     calculate_emerge_score(
         path_pattern,
-        args.output_uri,
         args.car_name,
         issue
     )
